b2189f36164049cf975a12475d8fdd6f6ba67f0a
krestosa <57479846+krestosa@users.noreply.github.com>
2026-07-05 04:47:13 -0300

    Add Project Graph watcher and cache

--- packages/shared/types/ipc.types.ts
+++ packages/shared/types/ipc.types.ts
@@ -1,27 +1,25 @@
 import type { ProjectGraph, ProjectScanResult } from "../../core/project/graph/project-graph.types";
+import type { ProjectGraphRefreshResult } from "../../core/project/refresh/project-graph-refresh.types";
+import type { ProjectWatcherState } from "../../core/project/watching/project-watch.types";
+import type { crystalIpcChannels } from "../constants/ipc.constants";
 
-export type CrystalIpcChannel =
-  | "app:get-version"
-  | "app:get-platform"
-  | "project:open-folder"
-  | "project:open-html-file"
-  | "project:scan"
-  | "project:get-graph";
+export type CrystalIpcChannel = typeof crystalIpcChannels[keyof typeof crystalIpcChannels];
 
 export interface CrystalIpcRequestMap {
-  readonly "app:get-version": void;
-  readonly "app:get-platform": void;
-  readonly "project:open-folder": void;
-  readonly "project:open-html-file": void;
-  readonly "project:scan": void;
-  readonly "project:get-graph": void;
+  readonly [channel: string]: void;
 }
 
 export interface CrystalIpcResponseMap {
+  readonly [channel: string]: unknown;
   readonly "app:get-version": string;
   readonly "app:get-platform": NodeJS.Platform;
   readonly "project:open-folder": ProjectScanResult | null;
   readonly "project:open-html-file": ProjectScanResult | null;
   readonly "project:scan": ProjectScanResult;
   readonly "project:get-graph": ProjectGraph | null;
+  readonly "project:start-watcher": ProjectWatcherState;
+  readonly "project:stop-watcher": ProjectWatcherState;
+  readonly "project:get-watcher-state": ProjectWatcherState;
+  readonly "project:refresh-graph": ProjectGraphRefreshResult;
+  readonly "project:clear-cache": ProjectWatcherState;
 }
