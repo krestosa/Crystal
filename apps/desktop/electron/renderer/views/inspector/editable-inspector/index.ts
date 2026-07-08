27bc7e48d529cf54cf23b7b09ae2ecbd9abb3a31
krestosa <57479846+krestosa@users.noreply.github.com>
2026-07-08 20:08:18 -0300

    Export editable inspector disabled surface

---
+++ apps/desktop/electron/renderer/views/inspector/editable-inspector/index.ts
@@ -0,0 +1,5 @@
+export * from "./editable-inspector.constants";
+export * from "./editable-inspector.model";
+export * from "./editable-inspector.render";
+export * from "./editable-inspector.types";
+export * from "./editable-inspector.validation";
